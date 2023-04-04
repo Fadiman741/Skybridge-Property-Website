8086b36c5370c0ddd7b0d654380247c87dec1e73
Mafadi Lephaka <mafadilephaka97@gmail.com>
2023-04-04 08:30:38 +0200

    pagination added

--- src/app/properties/properties/properties.component.ts
+++ src/app/properties/properties/properties.component.ts
@@ -8,6 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PropertiesComponent implements OnInit {
 
+  page: number = 1;
+  count: number = 0;
+  tableSize: number = 7;
+  tableSizes: any = [3, 6, 9, 12];
+
     propertyData:any=[
     { id:1,
       image:"./assets/profilepictures/mafadi.jpg",
@@ -80,6 +85,30 @@ export class PropertiesComponent implements OnInit {
       bathrooms:1,
       garage:1,
       size:1047
+    },
+    { id:5,
+      image:"fas fa-building",
+      name:"Rental Management",
+      price:"R 200 000",
+      province:"Gauteng",
+      address: "House 22 Stanza road",
+      description: "2 bathrooms Opee area Stacking doors open up to the swimmingpool area Study Kitchen with granite tops Seperate washingroom pen plan diingroom and lounge area Stacking doors open up to the swimmingpool area Study Kitchen with granite tops Seperate washingroom",
+      rooms:2,
+      bathrooms:1,
+      garage:1,
+      size:1047
+    },
+    { id:6,
+      image:"fas fa-building",
+      name:"Rental Management",
+      price:"R 200 000",
+      province:"Gauteng",
+      address: "House 22 Stanza road",
+      description: "2 bathrooms Opee area Stacking doors open up to the swimmingpool area Study Kitchen with granite tops Seperate washingroom pen plan diingroom and lounge area Stacking doors open up to the swimmingpool area Study Kitchen with granite tops Seperate washingroom",
+      rooms:2,
+      bathrooms:1,
+      garage:1,
+      size:1047
     }
   ];
 
@@ -87,6 +116,15 @@ export class PropertiesComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+  }
+  onTableDataChange(event: any) {
+    this.page = event;
+    this.propertyData;
+  }
+  onTableSizeChange(event: any): void {
+    this.tableSize = event.target.value;
+    this.page = 1;
+    this.propertyData;
   }
 
 }
--- src/app/properties/property.module.ts
+++ src/app/properties/property.module.ts
@@ -1,6 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PropertiesComponent } from './properties/properties.component';
+import {PaginationComponent} from './pagination/pagination.component';
+
+import { NgxPaginationModule } from 'ngx-pagination';
 
 
 import {PropertyroutingModule} from './property-routing.module';
@@ -8,8 +11,9 @@ import {PropertyroutingModule} from './property-routing.module';
 @NgModule({
   imports: [
     CommonModule,
-    PropertyroutingModule
+    PropertyroutingModule,
+    NgxPaginationModule
   ],
-  declarations: [PropertiesComponent]
+  declarations: [PropertiesComponent,PaginationComponent]
 })
 export class PropertyModule { }
